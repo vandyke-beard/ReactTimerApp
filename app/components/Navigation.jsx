eeca2449658b1e26c5060ed69358f4fac9cde4e9
Beard <f210686@gmail.com>
2016-09-14 11:04:17 +0800

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+var {Link , IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Timer App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Countdown</IndexLink>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Create by <a href="http://vandyke.tw/about#beard" target="_blank">Beard</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+module.exports = Navigation;
